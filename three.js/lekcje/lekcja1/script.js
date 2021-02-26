6556c0692142211b3b08b1769c0876ac1cd21233
MichaelMusiol1 <michaelmusiol77@gmail.com>
2021-02-26 15:39:27 +0100

    :)

--- three.js/lekcje/lekcja1/script.js
+++ three.js/lekcje/lekcja1/script.js
@@ -19,6 +19,7 @@ points.push( new THREE.Vector3(-10,0,0));
 //}
 
 
+
 const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
 const line = new THREE.Line(lineGeometry, LineMaterial);
 scene.add(line);
@@ -26,15 +27,15 @@ scene.add(line);
 const light1 = new THREE.PointLight({color:0xffffff})
 scene.add(light1);
 light1.position.x=50;
-light1.position.y=-20;
+light1.position.y=-30;
 light1.position.z=10;
 const light2= new THREE.PointLight({color:0xffffff})
 light2.position.set(-50,50,20)
 scene.add(light2);
-//const near = 80;
-//const far= 150;
+const near = 70;
+const far= 110;
 const color='black';
-//scene.fog = new THREE.Fog(color, near, far);
+scene.fog = new THREE.Fog(color, near, far);
 scene.background= new THREE.Color(color);
 const colorWhite = new THREE.Color('rgb(255, 255, 255)')
 
@@ -54,13 +55,41 @@ cube.position.x = 0;
 cube.position.y = 0;
 cube.position.z = 0;
 
+
+
+
 const animate = () =>{
 requestAnimationFrame(animate)
 cube.rotation.x +=-0.01
 cube.rotation.y +=-0.01
 cube.rotation.z +=-0.015
+
+renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.render(scene,camera)
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", renderer);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 scene.add(cube)
 renderer.render(scene, camera);
